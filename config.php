0be47c1e83288c5693dbc810db35d171200a8ef3
Edwin <edwin.h.morris@gmail.com>
2012-06-13 19:29:35 -0400

    updated config for php fog

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 $title = 'Class Search';
 
-$hostname = '127.0.0.1';
-$username = 'root';
-$password = '';
-$database = 'classes';
+$hostname = getenv('MYSQL_DB_HOST');
+$username = getenv('MYSQL_USERNAME');
+$password = getenv('MYSQL_PASSWORD');
+$database = getenv('MYSQL_DB_NAME');
 
 try {
   $connection = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
